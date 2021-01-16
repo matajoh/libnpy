814cece8d16890aef8f418e33a9aa9b606e1db05
Matthew Johnson <matjoh@microsoft.com>
2021-01-16 12:44:39 +0000

    These were included by mistake

--- test.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-color = np.arange(5*5*3).reshape(5, 5, 3).astype(np.uint8)
-depth = np.arange(5*5).reshape(5, 5).astype(np.float32)
-
-np.savez("test.npz", color=color, depth=depth)
